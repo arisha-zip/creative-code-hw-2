05c0a40ef0e578caac725b9367ed61ccc61386a1
arisha-zip <arisha.shumael@gwu.edu>
2026-06-24 17:26:28 -0700

    changing title card to gif

--- simple.js
+++ simple.js
@@ -36,7 +36,7 @@ let floatAmount = 10;
 function preload() {
   bg = loadImage("assets/finalpixelsky.png");
   g = loadImage("assets/grassblock.png");
-  titleCard = loadImage("assets/titledrawingNEW.png");
+  titleCard = loadImage("assets/titledrawingv3.gif");
   endCard = loadImage("assets/endcard.png");
   winCard = loadImage("assets/youwin.png");
 
